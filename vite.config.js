1fba79b2b0b789b5d1d2f54670ba36debb26222d
Zach <67765614+Zach488@users.noreply.github.com>
2022-10-18 07:15:31 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,3 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()]
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
-};
